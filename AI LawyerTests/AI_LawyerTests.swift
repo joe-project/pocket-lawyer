a8c0bb755dc7ba0e9649c696f776d07ff7e7ab18
Joseph Hernandez <josephdavidhernandez1@gmail.com>
2026-03-11 18:22:07 -0500

    Initial Commit

---
+++ AI LawyerTests/AI_LawyerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_LawyerTests.swift
+//  AI LawyerTests
+//
+//  Created by User on 3/11/26.
+//
+
+import Testing
+@testable import AI_Lawyer
+
+struct AI_LawyerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
